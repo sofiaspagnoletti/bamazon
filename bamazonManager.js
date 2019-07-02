90703ae928b59eabb93641e1fa7aa0c6398327c6
sofi.spag <sofi.spag@gmail.com>
2019-07-02 15:14:49 -0700

    adding low iventory and new product functions

--- bamazonManager.js
+++ bamazonManager.js
@@ -34,7 +34,7 @@ function runSearch() {
         "exit"
       ]
     })
-    .then(function(answer) {
+    .then((answer)=> {
       switch (answer.action) {
       case "View Products for Sale":
         viewProducts();
@@ -68,14 +68,58 @@ function viewProducts(params) {
       })
     };
 
-function viewLowInv(params) {
+function viewLowInv() {
+  connection.query("SELECT * FROM products where stock_quantity BETWEEN ? AND ?",[0, 5],function(err,data){
+      if (err) throw err;
+      console.table(data);
+      runSearch()
+  })
+}
+//if theres no product with low stock conosole log message 
+
+function addNewProduct(params) {
+  inquirer
+    .prompt([
+      {
+        name: "item",
+        type: "input",
+        message: "What is the name of product you would like to add?"
+      },
+      {
+        name: "category",
+        type: "input",
+        message: "What department/category would you like to place your product in?"
+      },
+      {
+        name: "price",
+        type: "input",
+        message: "What would you like the price to be?",
+      },
+      {
+        name: "quantity",
+        type: "input",
+        message: "How many items are you adding to your stock?",
+      }
+    ])
+    .then(function(answer) {
+      connection.query("INSERT INTO products SET ?",
+      {
+        product_name:answer.item,
+        department_name:answer.category,
+        price: answer.price,
+        stock_quantity: answer.quantity
+      },
+      function(err) {
+        if (err) throw err;
+        console.log("Your product successfully added!");
+        runSearch();
+      }
+      )
+    })
     
 }
 
 function addToInv(params) {
     
 }
-
-function addNewProduct(params) {
-    
-}
+// show table as a list and add quantity + number they wish to add 
