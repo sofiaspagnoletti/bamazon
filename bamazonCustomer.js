6b2887cef39b113161162aae4f195ca20ff361cc
sofi.spag <sofi.spag@gmail.com>
2019-07-01 21:20:07 -0700

    adding .gitignore

---
+++ bamazonCustomer.js
@@ -0,0 +1,65 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "Naon21552155",
+  database: "bamazon_db"
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  // run the start function after the connection is made to prompt the user
+  start();
+});
+
+function start() {
+  // query the database for all items being auctioned
+  connection.query("SELECT * FROM products", function (err, results) {
+    console.table(results);
+    if (err) throw err;
+    buyProd();
+  })
+};
+
+function buyProd() {
+  // once you have the items, prompt the user for which they'd like to bid on
+  inquirer
+    .prompt([
+      {
+        name: "selectProd",
+        type: "input",
+        message: "What's the item_ID of the product you would you like to buy?"
+      },
+      {
+        name: "selectQuantity",
+        type: "input",
+        message: "How many units of the product would you like to buy?"
+      }
+    ])
+    .then(function (answer) {
+        var selectProd = answer.selectProd;
+        var selectQuantity = parseInt(answer.selectQuantity);
+        connection.query("UPDATE products SET stock_quantity=stock_quantity-" + selectQuantity + " WHERE item_id= " + selectProd), function (err, data) {
+          if (err) throw err;
+        }
+        console.table(data);
+        console.log("Your order was successfully placed!");
+        connection.end();
+      
+        //show table with quantity reduced
+        //show total cost
+        //else Insufficient quantity!
+        
+
+    })
+
+}
---
+++ bamazonSeeds.sql
@@ -0,0 +1,35 @@
+USE bamazon_db;
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Echo (2nd Generation) - Smart speaker with Alexa", "Electronics", 69.99, 120);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Echo Dot (3rd Gen) - Smart speaker with Alexa", "Electronics", 24.99, 130);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Nintendo Switch", "Electronics", 250.99, 210);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Apple Iphone 6s", "Electronics", 199.00, 15);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Coffee Mug", "Home", 10.99, 55);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Coffeemaker", "Home", 98.99, 90);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Skillet with Glass Cover", "Home", 32.00, 90);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Men Sandals", "Clothing", 25.00, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Cotton Shirt", "Clothing", 26.00, 65);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Socks", "Clothing", 16.00, 85);
+
+USE bamazon_DB;
+SELECT* FROM products;
